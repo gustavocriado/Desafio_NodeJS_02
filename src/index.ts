cbb208757b519a7552bda8ae6bd837143d0b3b43
Gustavo Criado <gustavoliracriado30@gmail.com>
2022-03-17 18:12:34 -0300

    Desafio 2 concluido

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import {Aluno} from '../src/Models/Aluno';
 
 dotenv.config();
 
@@ -36,8 +37,48 @@ app.use(express.json());
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 
-
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
+
+	var primeiroAluno : Aluno = new Aluno("berna Genuino", 20, 7, 72.00, 1.72, true);
+	var segundoAluno : Aluno = new Aluno("pablo Silva", 29, 2, 81.00, 1.91, false);
+	var terceiroAluno : Aluno = new Aluno("rael Silva", 45, 9, 90.00, 1.69, true);
+	var alunos = new Array(primeiroAluno,segundoAluno,terceiroAluno);
+
+	//R01
+	var idadeSomada = primeiroAluno.idade + segundoAluno.idade + terceiroAluno.idade;
+
+	console.log('a idade de todos os ubuntus somada é: ' + idadeSomada + '\r\n')
 	
+	//R02
+	console.log('Agrupando o nome dos alunos\r\n')
+	var nomes = new Array(primeiroAluno.nome, segundoAluno.nome, terceiroAluno.nome);
+	nomes.forEach((value) => 
+	{
+		console.log(value);
+	})
+
+	//R03
+	console.log('Somando Imc dos alunos: \r\n')
+	alunos.forEach((value) => 
+	{
+		var imc = value.peso / (value.altura * value.altura);
+		console.log(imc.toFixed(2))		
+	})
+
+	//R04
+	console.log('Quantidade de ubuntus devs: \r\n')
+
+	var alunosEhDevs = alunos.filter((value) => value.ehDev)
+	alunosEhDevs.forEach((value) => {
+		console.log(value.nome);
+	})
+
+	//R05
+	console.log('Quantidade de ubuntus com Silva: \r\n')
+
+	var AlunosSilva = alunos.filter((value) => value.nome.includes("Silva"))
+	AlunosSilva.forEach((value) => {
+		console.log(value.nome)
+	})
 });
